18ea7fc16ff7fde66120b7c3dbbd46753fb784e2
Ergin Rusheku <rushekuergin@gmail.com>
2021-11-07 14:22:57 +0100

    y

---
+++ src/app/rental/rental-component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-rental',
+templateUrl: './rental-component.html',
+styleUrls: ['./rental-component.css']
+})
+
+
+
+export class RentalComponent {
+
+}
