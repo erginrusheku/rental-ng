a87b4e1cc83d0938cabf85ddb500f68812dbbc3e
Ergin Rusheku <rushekuergin@gmail.com>
2021-11-08 18:06:19 +0100

    kelvo shife si duket

--- src/app/rental/rental-component.ts
+++ src/app/rental/rental-component.ts
@@ -1,10 +1,11 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 
 @Component({
   selector: 'app-rental',
 templateUrl: './rental-component.html',
-styleUrls: ['./rental-component.css']
+styleUrls: ['./rental-component.css' ]
 })
 
 
